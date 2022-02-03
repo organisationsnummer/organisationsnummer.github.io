e514b42040eea6c6347d558a04a81a14f88b6be7
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2022-02-03 19:15:31 +0100

    Add undici

--- src/components/Implementations/index.js
+++ src/components/Implementations/index.js
@@ -13,6 +13,61 @@ const Implementations = (props) => (
             <th className="px-4 py-2 lowercase">Maintainer</th>
           </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td className="border px-4 py-2">
+              <a
+                className="text-blue-500 hover:underline"
+                rel="noopener noreferrer"
+                href="https://github.com/organisationsnummer/js"
+              >
+                JavaScript
+              </a>
+            </td>
+            <td className="border px-4 py-2">
+              <a
+                className="text-blue-500 hover:underline"
+                rel="noopener noreferrer"
+                href="https://github.com/organisationsnummer/js"
+              >
+                <img
+                  src="https://img.shields.io/github/v/release/organisationsnummer/js?style=flat-square"
+                  alt="GitHub release (latest by date)"
+                />
+              </a>
+            </td>
+            <td className="border px-4 py-2">
+              <a
+                className="text-blue-500 hover:underline"
+                rel="noopener noreferrer"
+                href="https://github.com/personnummer/meta/#package-specification-v1"
+              >
+                1.0
+              </a>
+            </td>
+            <td className="border px-4 py-2">
+              <a
+                className="text-blue-500 hover:underline"
+                rel="noopener noreferrer"
+                href="https://github.com/personnummer/js/actions"
+              >
+                <img
+                  src="https://img.shields.io/github/workflow/status/organisationsnummer/js/build?style=flat-square"
+                  alt="GitHub Workflow Status"
+                />
+              </a>
+            </td>
+            <td className="border px-4 py-2">
+              <a
+                className="text-blue-500 hover:underline"
+                rel="noopener noreferrer"
+                href="https://github.com/frozzare"
+              >
+                @frozzare
+              </a>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   </Block>
