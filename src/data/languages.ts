c3c2a565c86c196d7057ca8a8bc9abf36cdcb8a4
Fredrik Forsmo <frozzare@users.noreply.github.com>
2022-03-22 10:54:47 +0100

    Update languages.ts

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -8,8 +8,24 @@ type Languages = Language[];
 
 const languages: Languages = [
   {
+    name: 'C#',
+    link: 'https://github.com/organisationsnummer/csharp'
+  },
+  {
+    name: 'Dart',
+    link: 'https://github.com/organisationsnummer/dart'
+  },
+  {
+    name: 'Go',
+    link: 'https://github.com/organisationsnummer/go'
+  },
+  {
     name: 'JavaScript',
     link: 'https://github.com/organisationsnummer/js'
+  },
+  {
+    name: 'PHP',
+    link: 'https://github.com/organisationsnummer/php'
   }
 ];
 
