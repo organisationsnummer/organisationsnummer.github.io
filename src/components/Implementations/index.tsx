9eece01aa4d7c80c20383547a5313a3e54a1fa92
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2023-03-13 22:19:54 +0100

    Add dynamic list based on .meta.yaml

--- src/components/Implementations/index.tsx
+++ src/components/Implementations/index.tsx
@@ -1,284 +1,99 @@
 import Block, { BlockProps } from '../Block';
 
-const Implementations = (props: BlockProps) => (
+type Pkg = {
+  name: string;
+  branch: string;
+  spec: string;
+  maintainer: string;
+  repo: string;
+  workflow: string;
+};
+
+type ImplementationsProps = BlockProps & {
+  pkgs: Pkg[];
+};
+
+const Implementations = (props: ImplementationsProps) => (
   <Block {...props}>
     <div className="overflow-auto lg:overflow-visible">
       <table className="lg:table-fixed w-full">
         <thead>
           <tr>
             <th className="px-4 py-2 lowercase">Package</th>
-            <th className="px-4 py-2 lowercase">Pkg Version</th>
-            <th className="px-4 py-2 lowercase">Spec Version</th>
+            <th className="px-4 py-2 lowercase">Version</th>
+            <th className="px-4 py-2 lowercase">Specification</th>
             <th className="px-4 py-2 lowercase">Status</th>
             <th className="px-4 py-2 lowercase">Maintainer</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/dart"
-              >
-                Dart
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/dart"
-              >
-                <img
-                  src="https://img.shields.io/github/v/release/organisationsnummer/dart?style=flat-square"
-                  alt="GitHub release (latest by date)"
-                />
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/meta/#package-specification-v11"
-              >
-                1.1
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/dart/actions"
-              >
-                <img
-                  src="https://img.shields.io/github/workflow/status/organisationsnummer/dart/test?style=flat-square"
-                  alt="GitHub Workflow Status"
-                />
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/frozzare"
-              >
-                @frozzare
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/go"
-              >
-                Go
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/go"
-              >
-                <img
-                  src="https://img.shields.io/github/v/release/organisationsnummer/go?style=flat-square"
-                  alt="GitHub release (latest by date)"
-                />
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/meta/#package-specification-v11"
-              >
-                1.1
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/go/actions"
-              >
-                <img
-                  src="https://img.shields.io/github/workflow/status/organisationsnummer/go/test?style=flat-square"
-                  alt="GitHub Workflow Status"
-                />
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/frozzare"
-              >
-                @frozzare
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/js"
-              >
-                JavaScript
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/js"
-              >
-                <img
-                  src="https://img.shields.io/github/v/release/organisationsnummer/js?style=flat-square"
-                  alt="GitHub release (latest by date)"
-                />
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/personnummer/meta/#package-specification-v11"
-              >
-                1.1
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/personnummer/js/actions"
-              >
-                <img
-                  src="https://img.shields.io/github/workflow/status/organisationsnummer/js/build?style=flat-square"
-                  alt="GitHub Workflow Status"
-                />
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/frozzare"
-              >
-                @frozzare
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/csharp"
-              >
-                C#
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/csharp"
-              >
-                <img
-                  src="https://img.shields.io/github/v/release/organisationsnummer/csharp?style=flat-square"
-                  alt="GitHub release (latest by date)"
-                />
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/personnummer/meta/#package-specification-v11"
-              >
-                1.1
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/personnummer/csharp/actions"
-              >
-                <img
-                  src="https://img.shields.io/github/workflow/status/organisationsnummer/csharp/Release?style=flat-square"
-                  alt="GitHub Workflow Status"
-                />
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/Johannestegner"
-              >
-                @Johannestegner
-              </a>
-            </td>            
-          </tr>
-          <tr>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/php"
-              >
-                PHP
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/organisationsnummer/php"
-              >
-                <img
-                  src="https://img.shields.io/github/v/release/organisationsnummer/php?style=flat-square"
-                  alt="GitHub release (latest by date)"
-                />
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/personnummer/meta/#package-specification-v11"
-              >
-                1.1
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/personnummer/php/actions"
-              >
-                <img
-                  src="https://img.shields.io/github/workflow/status/organisationsnummer/php/PHP%20Composer?style=flat-square"
-                  alt="GitHub Workflow Status"
-                />
-              </a>
-            </td>
-            <td className="border px-4 py-2">
-              <a
-                className="text-blue-500 hover:underline"
-                rel="noopener noreferrer"
-                href="https://github.com/Johannestegner"
-              >
-                @Johannestegner
-              </a>
-            </td>
-          </tr>
+          {props.pkgs?.map((pkg) => (
+            <tr key={pkg.repo}>
+              <td className="border px-4 py-2">
+                <a
+                  className="text-blue-500 hover:underline"
+                  rel="noopener noreferrer"
+                  href={pkg.repo}
+                >
+                  {pkg.name}
+                </a>
+              </td>
+              <td className="border px-4 py-2">
+                <a
+                  className="text-blue-500 hover:underline"
+                  rel="noopener noreferrer"
+                  href={pkg.repo}
+                >
+                  <img
+                    src={`https://img.shields.io/github/v/release/personnummer/${pkg.repo
+                      .split('/')
+                      .pop()}?style=flat-square`}
+                    alt="GitHub release (latest by date)"
+                  />
+                </a>
+              </td>
+              <td className="border px-4 py-2">
+                <a
+                  className="text-blue-500 hover:underline"
+                  rel="noopener noreferrer"
+                  href={`https://github.com/personnummer/meta/#package-specification-v${pkg.spec
+                    .replace(/(\d).0/, '$1')
+                    .replace('.', '')}`}
+                >
+                  <img
+                    src={`https://img.shields.io/static/v1?label=spec&message=${pkg.spec}&color=blue&style=flat-square`}
+                    alt="Specification version"
+                  />
+                </a>
+              </td>
+              <td className="border px-4 py-2">
+                <a
+                  className="text-blue-500 hover:underline"
+                  rel="noopener noreferrer"
+                  href={`${pkg.repo}/actions`}
+                >
+                  <img
+                    src={`https://img.shields.io/github/actions/workflow/status/${pkg.repo
+                      .split('/')
+                      .slice(-2)
+                      .join('/')}/${pkg.workflow}?branch=${
+                      pkg.branch
+                    }&style=flat-square`}
+                    alt="GitHub Workflow Status"
+                  />
+                </a>
+              </td>
+              <td className="border px-4 py-2">
+                <a
+                  className="text-blue-500 hover:underline"
+                  rel="noopener noreferrer"
+                  href={`https://github.com/${pkg.maintainer}`}
+                >
+                  {pkg.maintainer}
+                </a>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
